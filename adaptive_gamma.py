0588563ce1a9c7d433eb41396adb4d0934e216d7
ssszZ_window11 <spachitralada@gmail.com>
2023-06-23 14:44:16 +0700

    เปลี่ยนให้ program อ่านภาพในรุปเเบบ gray scale เเท

--- adaptive_gamma.py
+++ adaptive_gamma.py
@@ -3,18 +3,18 @@ import numpy as np
 import power_law
 
 path_to_img = "./18558.jpg"
-img = cv.imread(path_to_img)
-img_h, img_w, _ = img.shape
+img = cv.imread(path_to_img,cv.IMREAD_GRAYSCALE)
+img_h, img_w = img.shape
 split_width = 500
 split_height = 500
-overlap_size = 10
 
-def start_points(size, split_size, overlap=overlap_size):
+
+def start_points(size, split_size, overlap):
     points = [0]
     stride = int(split_size * (1-overlap))
     counter = 1
     while True:
-        pt = stride * counter
+        pt = stride * counter # หาตำเเหน่งที่ ploter จะเข้าไป plot 
         if pt + split_size >= size:
             if split_size == size:
                 break
@@ -29,12 +29,13 @@ def start_points(size, split_size, overlap=overlap_size):
 X_points = start_points(img_w, split_width, 0.5)
 Y_points = start_points(img_h, split_height, 0.5)
 
-count = 0 #เอาไว้ยัดในชื่อรูป เพราะชื่อรูปซ้ำกันไม่ได้
-name = 'splitted'
-frmt = 'png'
+#count = 0 #เอาไว้ยัดในชื่อรูป เพราะชื่อรูปซ้ำกันไม่ได้
+#name = 'splitted'
+#frmt = 'png'
 
 for i in Y_points:
     for j in X_points:
         split = img[i:i+split_height, j:j+split_width]
-        cv.imwrite('{}_{}.{}'.format(name, count, frmt), split)
-        count += 1
+#         cv.imwrite('{}_{}.{}'.format(name, count, frmt), split)
+#         count += 1
+print(split.shape)
