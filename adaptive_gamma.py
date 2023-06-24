1566e0f09130d815f1104350f7eb46caf8e9ecbd
ssszZ_window11 <spachitralada@gmail.com>
2023-06-24 09:40:36 +0700

    เส็ดเเล้ว test เเล้วใช้ได้จริง

--- adaptive_gamma.py
+++ adaptive_gamma.py
@@ -9,8 +9,8 @@ img_h, img_w = img.shape
 split_width = 500
 split_height = 500
 dark_zone = 128
-gamma_light_up = 0.5
-gamma_light_down = 1.5
+gamma_light_up = 0.2
+gamma_light_down = 1.8
 
 def tune_light(img_in, light_up=True):
     if light_up==True:
@@ -19,6 +19,7 @@ def tune_light(img_in, light_up=True):
         gamma_corrected = (img_in / 255) ** gamma_light_down
         
     gamma_corrected = gamma_corrected*255
+    return gamma_corrected
 
 def start_points(size, split_size, overlap):
     points = [0]
@@ -48,15 +49,20 @@ for i in Y_points:
     for j in X_points:
         splited = img[i:i+split_height, j:j+split_width]
         histrogram = cv.calcHist([splited],[0],None,[256],[128,256])
-        if histrogram.mean() <= dark_zone :
+        histrogram_mean = histrogram.mean()
+        
+        if histrogram_mean <= dark_zone :
             print("up_light", end="\t\t")
+            splited_tuned=tune_light(splited,light_up=True)
         else :
             print("dn_light", end="\t\t")
-        #cv.imwrite('{}_{}.{}'.format(name, count, frmt), split) ##debug ว่าภาพเเบ่งจริงอะป่าว
-        #count += 1
-        # plt.plot(histrogram)#debug ว่าทำไมค่า mean sum ออกมาเท่ากันทุกตัวเลย
-        # plt.title('Histogram for gray scale image')
-        # plt.show()
+            splited_tuned=tune_light(splited,light_up=False)
+        
+        cv.imwrite('{}_{}.{}'.format(name, count, frmt), splited_tuned) ##debug ว่าภาพเเบ่งจริงอะป่าว
+        count += 1
+        plt.plot(histrogram)#debug ว่าทำไมค่า mean sum ออกมาเท่ากันทุกตัวเลย
+        plt.title('spa AI histrogram mean =>> {}'.format(histrogram_mean))
+        plt.show()
     print("\n")
         
 #print(all_grayscale)
