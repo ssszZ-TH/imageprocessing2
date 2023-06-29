6ccc92e93b6854c1a515fd56cad32630bf2498f2
ssszZ window11_tufA15 <spachitralada@gmail.com>
2023-06-29 13:33:39 +0700

    create adaptive gamma v2

--- power_law.py
+++ power_law.py
@@ -2,11 +2,8 @@ import cv2 as cv
 import numpy as np
 
 def power_law(img_in,gamma): # ที่อยู่ไฟล์ gamma
-    
     gamma_corrected = (img_in / 255) ** gamma
-
     gamma_corrected = gamma_corrected*255
-
     return np.array(gamma_corrected , dtype="uint8")
 
     
@@ -14,6 +11,5 @@ def power_law(img_in,gamma): # ที่อยู่ไฟล์ gamma
 if __name__  == "__main__":
     part="./18558.jpg"
     img_in = cv.imread(part,cv.IMREAD_GRAYSCALE)
-    img_out=power_law(img_in,0.2)
-    cv.imshow("sitthipong" , img_out)
+    img_out = power_law(img_in,0.2)
     cv.imwrite("demo1.png" , img_out)
