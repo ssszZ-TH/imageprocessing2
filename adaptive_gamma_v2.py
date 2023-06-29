64116bfc9f202d5bbfbb71b4e8fb88a31e6def02
ssszZ window11_tufA15 <spachitralada@gmail.com>
2023-06-29 22:24:16 +0700

    test ผ่าน

--- adaptive_gamma_v2.py
+++ adaptive_gamma_v2.py
@@ -10,10 +10,11 @@ if __name__=="__main__":
     path_to_img = "./dif_shade.jpeg"
     img = cv.imread(path_to_img,cv.IMREAD_GRAYSCALE)
     img = np.array(img, dtype="uint8")
-    split_size = 500
-    dark_zone = 128
-    gamma_light_up = 0.2
-    gamma_light_down = 1.8
+    split_size = 50
+    overlap=25
+    dark_zone = 2
+    gamma_light_up = 0.5
+    gamma_light_down = 1.5
     
     
     ## img[0:500,0:500] #[y:y , x:x] 
@@ -30,6 +31,8 @@ if __name__=="__main__":
             ##เเต่งรูปเล็กๆ เเต่ละรูป
             histrogram = cv.calcHist([img_],[0],None,[256],[128,256])
             histrogram_mean = histrogram.mean()
+            print(histrogram_mean)
+            
             if histrogram_mean<=dark_zone:
                 img_=power_law(img_,gamma_light_up)
             else :
