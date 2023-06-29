5b598261f7c74cc786dc488189af73d80cb083ee
ssszZ window11_tufA15 <spachitralada@gmail.com>
2023-06-29 17:02:45 +0700

    ทำ base เส็ด กำลังจะทำ loop check เเบ่งรูป

--- adaptive_gamma_v2.py
+++ adaptive_gamma_v2.py
@@ -9,12 +9,17 @@ if __name__=="__main__":
     path_to_img = "./dif_shade.jpeg"
     img = cv.imread(path_to_img,cv.IMREAD_GRAYSCALE)
     img = np.array(img, dtype="uint8")
-    img_h, img_w = img.shape
     split_size = 500
     dark_zone = 128
     gamma_light_up = 0.2
     gamma_light_down = 1.8
     
-    img = img[0:500,0:500]
+    
+    #img = img[0:500,0:500] #[y:y , x:x] 
+    img_h, img_w = img.shape
+    step_y = img_h//split_size
+    step_x = img_w//split_size
+    
+    
     
     
