a677bced7c31c9037a61b6ddc4c4fff6f0ec8240
ssszZ window11_tufA15 <spachitralada@gmail.com>
2023-06-29 21:55:20 +0700

    เเบ่งภาพเเต่ละรูปมาเเสดงสำเร็จ

--- adaptive_gamma_v2.py
+++ adaptive_gamma_v2.py
@@ -9,53 +9,19 @@ if __name__=="__main__":
     path_to_img = "./dif_shade.jpeg"
     img = cv.imread(path_to_img,cv.IMREAD_GRAYSCALE)
     img = np.array(img, dtype="uint8")
-    split_size = 250
+    split_size = 500
     dark_zone = 128
     gamma_light_up = 0.2
     gamma_light_down = 1.8
     
     
-    #img = img[0:500,0:500] #[y:y , x:x] 
-    img_h, img_w = img.shape
-    step_y = img_h//split_size
-    step_x = img_w//split_size
-    startx_=0
-    starty_=0
-    endx_=split_size
-    endy_=split_size
-    print("image size x,y =",img_w,img_h)
-    print("step x y = ", step_x, step_y)
-    for y in range(step_y):
-        
-        #กัน focus หลุดกรอบรูป
-        if endy_>img_h:
-            break
-            
-        for x in range(step_x):
-            
-            # กัน focus หลุดกรอบรุป
-            if endx_>img_w:
-                continue
-            
-            # เเบ่งรูป ตามเเนว [y ถึง y , x ถึง x]
-            img_splited = img[starty_:endy_ , startx_:endx_]
-            print("now focus on (x-x,y-y)=",startx_,endx_,"   ",starty_,endy_)
-            
-            #shift ตำเเหน่ง focusไปทาง x+
-            startx_+=split_size
-            endx_+=split_size
-            
-            #เเสดงรูป
-            cv.imshow('graycsale image',img_splited)
+    ## img[0:500,0:500] #[y:y , x:x] 
+    
+    img_out = np.zeros(img.shape,dtype="uint8")
+ 
+    for r in range(0, img.shape[0] ,split_size):
+        for c in range(0, img.shape[1] ,split_size):
+            cv.imshow("slice",img[r:r+split_size, c:c+split_size])
             cv.waitKey(0)
             cv.destroyAllWindows()
             
-        # shift y ไปทาง y+
-        starty_+=split_size
-        endy_+=split_size
-        
-        # shift x focus ให้กลับไปมองตรง ซ้ายสุดอีกรอบ
-        startx_=0
-        starty_=split_size
-        
-        print("ทำตรงนี้")
