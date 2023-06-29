97996ec6aeea4635a8e54783a39f6ac89d81d781
ssszZ window11_tufA15 <spachitralada@gmail.com>
2023-06-29 13:30:11 +0700

    delete trash photo

--- power_law.py
+++ power_law.py
@@ -7,13 +7,13 @@ def power_law(img_in,gamma): # ที่อยู่ไฟล์ gamma
 
     gamma_corrected = gamma_corrected*255
 
-    img_out = np.array(gamma_corrected , dtype="uint8")
+    return np.array(gamma_corrected , dtype="uint8")
 
-    cv.imshow("sitthipong" , img_out)
-
-    cv.imwrite("demo1.png" , img_out)
+    
     
 if __name__  == "__main__":
     part="./18558.jpg"
     img_in = cv.imread(part,cv.IMREAD_GRAYSCALE)
-    power_law(img_in,0.2)
+    img_out=power_law(img_in,0.2)
+    cv.imshow("sitthipong" , img_out)
+    cv.imwrite("demo1.png" , img_out)
